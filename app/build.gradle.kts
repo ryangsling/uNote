c3bb5c414be24b707aecbb8029b8f0d3c0a2bfb7
ryangsling <jamesahsan1371@gmail.com>
2026-07-21 00:24:47 +0600

    mycommit

---
+++ app/build.gradle.kts
@@ -0,0 +1,10 @@
+defaultConfig {
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+}
+
+dependencies {
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test:runner:1.6.2")
+    androidTestImplementation("androidx.test:rules:1.6.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+}
